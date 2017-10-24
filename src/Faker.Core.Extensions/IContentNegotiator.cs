b8724c2aac5e9906c3964c0f429750849ce6b64c
Jordan anderson <jordan_anders@live.co.uk>
2017-10-24 20:27:38 +0100

    adds interface for content negotiator

---
+++ src/Faker.Core.Extensions/IContentNegotiator.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Faker.Core.Extensions {
+    public interface IContentNegotiator
+    {
+        IRequest Negotiate(string requestContent, Dictionary<string, string> metadata);
+    }
+}
