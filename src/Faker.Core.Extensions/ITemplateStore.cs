83df701dcb328e05911694e107def9c563c7ccd3
Jordan anderson <jordan_anders@live.co.uk>
2017-10-29 11:58:19 +0000

    Changes storage methods to asynchronous operations

--- src/Faker.Core.Extensions/ITemplateStore.cs
+++ src/Faker.Core.Extensions/ITemplateStore.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Faker.Core.Extensions {
     public interface ITemplateStore<T> where T : ITemplate
     {
-        ITemplateContainer<T> GetTemplateContainer(Uri @namespace);
+        Task<ITemplateContainer<T>> GetTemplateContainerAsync(Uri @namespace, CancellationToken cancellationToken);
 
-        void StoreTemplate(Uri @namespace, T template);
+        Task StoreTemplateAsync(Uri @namespace, T template, CancellationToken cancellationToken);
+        Task RemoveTemplateAsync(Uri @namespace, Guid id, CancellationToken cancellationToken);
+        Task UpdateTemplateAsync(Uri @namespace, Guid id, T template, CancellationToken cancellationToken);
+        Task DeleteContainerAsync(Uri @namespace, CancellationToken cancellationToken);
+
+
     }
 }
